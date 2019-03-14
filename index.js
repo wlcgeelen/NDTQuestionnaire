dc390f5168bf9b0efcee2e7bbd03a5a2c4656e35
wlcgeelen <47600031+wlcgeelen@users.noreply.github.com>
2019-03-14 11:20:19 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,24 +1,37 @@
-SurveyEditor.StylesManager.applyTheme("bootstrap");
-
-var editorOptions = {
-    questionTypes: ["text", "checkbox", "radiogroup", "dropdown", "nouislider"],
-    // show options menu
-    showOptions: true,
-    showJSONEditorTab: false
+// LOAD SURVEY
+var surveyJSON = { title: "Tell us, what technologies do you use?", pages: [
+  { name:"page1", questions: [ 
+      { type: "radiogroup", choices: [ "Yes", "No" ], isRequired: true, name: "frameworkUsing",title: "Do you use any front-end framework like Bootstrap?" },
+      { type: "checkbox", choices: ["Bootstrap","Foundation"], hasOther: true, isRequired: true, name: "framework", title: "What front-end framework do you use?", visibleIf: "{frameworkUsing} = 'Yes'" }
+   ]},
+  { name: "page2", questions: [
+    { type: "radiogroup", choices: ["Yes","No"],isRequired: true, name: "mvvmUsing", title: "Do you use any MVVM framework?" },
+    { type: "checkbox", choices: [ "AngularJS", "KnockoutJS", "React" ], hasOther: true, isRequired: true, name: "mvvm", title: "What MVVM framework do you use?", visibleIf: "{mvvmUsing} = 'Yes'" } ] },
+  { name: "page3",questions: [
+    { type: "comment", name: "about", title: "Please tell us about your main requirements for Survey library" } ] }
+ ]
 };
 
-var editor = new SurveyEditor.SurveyEditor("editorElement", editorOptions);
+//You should get the Guid for storing survey data in surveyjs.io service storage
+survey.surveyPostId = 'e544a02f-7fff-4ffb-b62d-6a9aa16efd7c';
 
-//Setting this callback will make visible the "Save" button
-editor.saveSurveyFunc = function () {
-    //save the survey JSON
-    var jsonEl = document.getElementById('surveyJSON');
-    jsonEl.value = editor.text;
+// Define survey data safe function.
+function sendDataToServer(survey) {
+  survey.sendResult('e544a02f-7fff-4ffb-b62d-6a9aa16efd7c');
+  var resultAsString = JSON.stringify(survey.data);
+  alert(resultAsString); //send Ajax request to your web server.
 }
 
-editor.text = MySurveyJSON;
 
-//set function on save callback
-editor.saveSurveyFunc = saveMySurvey;
+// APPLY Bootstrap theme
+Survey.StylesManager.applyTheme("bootstrap");
 
+var surveyWindow = new Survey.SurveyWindow(surveyJSON);
+surveyWindow.show();
+//Use onComplete event to save the data
+surveyWindow.survey.onComplete.add(sendDataToServer);
+
+var survey = new Survey.Model(surveyJSON, "surveyContainer");
+//Use onComplete event to save the data
+survey.onComplete.add(sendDataToServer);
 
