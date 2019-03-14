5cd9d6eeb924f62476cb81749a2454d430ac7794
wlcgeelen <47600031+wlcgeelen@users.noreply.github.com>
2019-03-14 12:57:07 +0100

    Update index.js

--- index.js
+++ index.js
@@ -4,103 +4,123 @@ Survey
 Survey.defaultBootstrapCss.navigationButton = "btn btn-green";
 
 var json = {
-    pages: [
-        {
-            questions: [
-                {
-                    type: "matrix",
-                    name: "Quality",
-                    title: "Please indicate if you agree or disagree with the following statements",
-                    columns: [
-                        {
-                            value: 1,
-                            text: "Strongly Disagree"
-                        }, {
-                            value: 2,
-                            text: "Disagree"
-                        }, {
-                            value: 3,
-                            text: "Neutral"
-                        }, {
-                            value: 4,
-                            text: "Agree"
-                        }, {
-                            value: 5,
-                            text: "Strongly Agree"
-                        }
-                    ],
-                    rows: [
-                        {
-                            value: "affordable",
-                            text: "Product is affordable"
-                        }, {
-                            value: "does what it claims",
-                            text: "Product does what it claims"
-                        }, {
-                            value: "better then others",
-                            text: "Product is better than other products on the market"
-                        }, {
-                            value: "easy to use",
-                            text: "Product is easy to use"
-                        }
-                    ]
-                }, {
-                    type: "rating",
-                    name: "satisfaction",
-                    title: "How satisfied are you with the Product?",
-                    isRequired: true,
-                    mininumRateDescription: "Not Satisfied",
-                    maximumRateDescription: "Completely satisfied"
-                }, {
-                    type: "rating",
-                    name: "recommend friends",
-                    visibleIf: "{satisfaction} > 3",
-                    title: "How likely are you to recommend the Product to a friend or co-worker?",
-                    mininumRateDescription: "Will not recommend",
-                    maximumRateDescription: "I will recommend"
-                }, {
-                    type: "comment",
-                    name: "suggestions",
-                    title: "What would make you more satisfied with the Product?"
-                }
-            ]
-        }, {
-            questions: [
-                {
-                    type: "radiogroup",
-                    name: "price to competitors",
-                    title: "Compared to our competitors, do you feel the Product is",
-                    choices: ["Less expensive", "Priced about the same", "More expensive", "Not sure"]
-                }, {
-                    type: "radiogroup",
-                    name: "price",
-                    title: "Do you feel our current price is merited by our product?",
-                    choices: ["correct|Yes, the price is about right", "low|No, the price is too low for your product", "high|No, the price is too high for your product"]
-                }, {
-                    type: "multipletext",
-                    name: "pricelimit",
-                    title: "What is the... ",
-                    items: [
-                        {
-                            name: "mostamount",
-                            title: "Most amount you would every pay for a product like ours"
-                        }, {
-                            name: "leastamount",
-                            title: "The least amount you would feel comfortable paying"
-                        }
-                    ]
-                }
-            ]
-        }, {
-            questions: [
-                {
-                    type: "text",
-                    name: "email",
-                    title: "Thank you for taking our survey. Your survey is almost complete, please enter your email address in the box below if you wish to participate in our drawing, then press the 'Submit' button."
-                }
-            ]
-        }
-    ]
+ pages: [
+  {
+   name: "Introduction",
+   elements: [
+    {
+     type: "html",
+     name: "question1"
+    }
+   ]
+  },
+  {
+   name: "Ranking",
+   elements: [
+    {
+     type: "matrixdropdown",
+     name: "Ordinal ranking of attributes",
+     columns: [
+      {
+       name: "Attribute rank",
+       isRequired: true
+      }
+     ],
+     choices: [
+      1,
+      2,
+      3,
+      4,
+      5,
+      6,
+      7,
+      8,
+      9,
+      10,
+      11
+     ],
+     rows: [
+      "FOV",
+      "Range",
+      "Accuracy",
+      "Frame rate",
+      "Resolution",
+      "Color perception",
+      "Size",
+      "Weather affection",
+      "Maintenance",
+      "Visibility",
+      "Price"
+     ]
+    }
+   ]
+  },
+  {
+   name: "LowerBound",
+   elements: [
+    {
+     type: "text",
+     name: "question2",
+     title: "Assing a value in the range 0-100, to the attribute ranked lowest on the previous page",
+     isRequired: true,
+     validators: [
+      {
+       type: "numeric",
+       minValue: 0,
+       maxValue: 100
+      }
+     ],
+     inputType: "number"
+    }
+   ]
+  },
+  {
+   name: "RelativeImportance",
+   elements: [
+    {
+     type: "multipletext",
+     name: "question3",
+     title: "Please give the following attributes a values from 0 - 100",
+     isRequired: true,
+     items: [
+      {
+       name: "FOV"
+      },
+      {
+       name: "Range"
+      },
+      {
+       name: "Accuracy"
+      },
+      {
+       name: "Frame rate"
+      },
+      {
+       name: "Resolution"
+      },
+      {
+       name: "Color perception"
+      },
+      {
+       name: "Size"
+      },
+      {
+       name: "Weather affection"
+      },
+      {
+       name: "Maintenance"
+      },
+      {
+       name: "Visibility"
+      },
+      {
+       name: "Price"
+      }
+     ]
+    }
+   ]
+  }
+ ]
 };
 
 window.survey = new Survey.Model(json);
