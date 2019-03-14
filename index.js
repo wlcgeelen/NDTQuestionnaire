b746464b1a4408046e07b5affdf4848c77cfda9e
wlcgeelen <47600031+wlcgeelen@users.noreply.github.com>
2019-03-14 09:52:18 +0100

    Update index.js

--- index.js
+++ index.js
@@ -6,3 +6,12 @@ var editor = new SurveyEditor.SurveyEditor("editorElement", editorOptions);
 SurveyEditor
     .StylesManager
     .applyTheme("bootstrap");
+
+//Setting this callback will make visible the "Save" button
+editor.saveSurveyFunc = function () {
+    //save the survey JSON
+    var jsonEl = document.getElementById('surveyJSON');
+    jsonEl.value = editor.text;
+}
+
+editor.text = "{ pages: [{ name:\'page1\', questions: [{ type: \'text\', name:\"q1\"}]}]}";
